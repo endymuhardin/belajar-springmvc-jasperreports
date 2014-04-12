17dd0806ccf41d5edbcf646aa50458cfbd130d8f
Endy Muhardin <endy.muhardin@gmail.com>
2014-04-12 10:22:52 +0700

    enable jasper report

--- src/main/java/com/muhardin/endy/belajar/springmvcjasper/controller/HaloController.java
+++ src/main/java/com/muhardin/endy/belajar/springmvcjasper/controller/HaloController.java
@@ -1,5 +1,7 @@
 package com.muhardin.endy.belajar.springmvcjasper.controller;
 
+import java.util.Date;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +9,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HaloController {
-    
+
     @RequestMapping(value = "/halo", method = RequestMethod.GET)
-    public ModelMap halo(){ 
-        return new ModelMap().addAttribute("nama", "Endy Muhardin"); 
+    public ModelMap halo() {
+        return new ModelMap().addAttribute("nama", "Endy Muhardin");
     }
-    
+
+    @RequestMapping(value = "/anggota/export*", method = RequestMethod.GET)
+    public ModelMap exportDataAnggota(HttpServletRequest request) {
+        String uri = request.getRequestURI();
+        String format = uri.substring(uri.lastIndexOf(".") + 1);
+
+        return new ModelMap()
+                .addAttribute("format", format)
+                .addAttribute("tanggal", new Date());
+    }
 }
